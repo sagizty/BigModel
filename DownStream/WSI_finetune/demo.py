d64c89452c172a495970a55707487e99a03c2305
Tianyi Zhang <50575108+sagizty@users.noreply.github.com>
2024-08-21 01:17:56 +0800

    Update demo.py

--- DownStream/WSI_finetune/demo.py
+++ DownStream/WSI_finetune/demo.py
@@ -1,9 +1,10 @@
 '''
-MTL training panel       Script  ver： Aug 21th 00:00
+MTL training panel       Script  ver： Aug 21th 01:00
 '''
 import os
 import sys
 from pathlib import Path
+
 # For convinience
 this_file_dir = Path(__file__).resolve().parent
 sys.path.append(str(this_file_dir.parent.parent.parent))  # Go up 3 levels
@@ -20,9 +21,11 @@ from utils import seed_torch, get_exp_code, get_splits, get_loader
 try:
     from DataPipe.slide_dataset_tools import *
     from ModelBase.Get_WSI_model import build_WSI_task_model
+    from ModelBase.Task_settings import *
 except:
     from PuzzleAI.DataPipe.slide_dataset_tools import *
     from PuzzleAI.ModelBase.Get_WSI_model import build_WSI_task_model
+    from PuzzleAI.ModelBase.Task_settings import *
 
 if __name__ == '__main__':
     root_path = '/data/BigModel/embedded_datasets/'
@@ -31,11 +34,12 @@ if __name__ == '__main__':
     slide_id_key = 'patient_id'
     split_target_key = 'fold_information'
     task_setting_folder_name = 'task-settings'
+    latent_feature_dim = 128
 
     build_data_split_for_csv(task_description_csv, slide_id_key=slide_id_key, test_ratio=0.2, k=1,
                              mode='TCGA', key=split_target_key)
-    output_dir = os.path.join(root_path, task_setting_folder_name)
-    build_yaml_config_from_csv(task_description_csv, output_dir, dataset_name='lung-mix',
+    task_settings_path = os.path.join(root_path, task_setting_folder_name)
+    build_yaml_config_from_csv(task_description_csv, task_settings_path, dataset_name='lung-mix',
                                tasks_to_run=['CMS', 'COL3A1'],
                                max_tiles=1000000, shuffle_tiles=True,
                                excluding_list=(slide_id_key, split_target_key))
@@ -45,7 +49,7 @@ if __name__ == '__main__':
                                       split_name='train', slide_id_key=slide_id_key, split_target_key=split_target_key)
 
     print('get sample')
-    sample= DatasetClass_train.get_embedded_sample_with_try(20)
+    sample = DatasetClass_train.get_embedded_sample_with_try(20)
     # print(sample)
     '''
     sample = {'image_features': image features [N, D] tensor,
@@ -57,8 +61,15 @@ if __name__ == '__main__':
               'task_description_list': task_description_list}
     '''
 
+    task_config_path = os.path.join(task_settings_path, 'task_configs.yaml')
+    WSI_task_dict, MTL_heads, WSI_criterions, loss_weight, class_num, WSI_task_describe = \
+        task_filter_auto(task_config_path, latent_feature_dim=latent_feature_dim)
+    print('WSI_task_dict', WSI_task_dict)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = build_WSI_task_model(model_name='gigapath', local_weight_path=None, ROI_feature_dim=1536)
+
+    model = build_WSI_task_model(model_name='gigapath', local_weight_path=None, ROI_feature_dim=1536,
+                                 MTL_heads=MTL_heads,latent_feature_dim=latent_feature_dim)
     model = model.to(device)
 
     # example with a sample
@@ -66,7 +77,7 @@ if __name__ == '__main__':
     images, img_coords, task_description_list = \
         sample['image_features'], sample['coords_yx'], sample['task_description_list']
     # stack to a pseudo batch
-    images=images.unsqueeze(0)
+    images = images.unsqueeze(0)
     img_coords = img_coords.unsqueeze(0)
 
     images = images.to(device, non_blocking=True)
@@ -74,7 +85,7 @@ if __name__ == '__main__':
     # label = label.to(device, non_blocking=True).long()
 
     with torch.cuda.amp.autocast(dtype=torch.float16):
-        slide_embeds = model(images, img_coords, all_layer_embed=True)
-    outputs = {"layer_{}_embed".format(i): slide_embeds[i].cpu() for i in range(len(slide_embeds))}
-    outputs["last_layer_embed"] = slide_embeds[-1].cpu()
-    print(outputs)
+        slide_embeds = model(images, img_coords)
+    # layer_outputs = {"layer_{}_embed".format(i): slide_embeds[i].cpu() for i in range(len(slide_embeds))}
+    # layer_outputs["last_layer_embed"] = slide_embeds[-1].cpu()
+    print(slide_embeds)
