72e51aa6700b148a3d8aa2ed2ef128a596e4ba26
Tianyi Zhang <50575108+sagizty@users.noreply.github.com>
2024-08-21 01:17:35 +0800

    Script  ver： Aug 21th 01:00

--- ModelBase/Get_WSI_model.py
+++ ModelBase/Get_WSI_model.py
@@ -1,22 +1,31 @@
 """
-Build WSI level models     Script  ver： Aug 17th 12:00
+Build WSI level models     Script  ver： Aug 21th 01:00
 """
-from ModelBase.gigapath.slide_encoder import *
+import os
 import torch
+import torch.nn as nn
+from typing import Optional, List
 import huggingface_hub
 
-def build_WSI_backbone_model(model_name='gigapath',local_weight_path=None,ROI_feature_dim=1536, **kwargs):
+try:
+    from gigapath.slide_encoder import gigapath_slide_enc12l768d
+except:
+    from PuzzleAI.ModelBase.gigapath.slide_encoder import gigapath_slide_enc12l768d
 
+
+def build_WSI_backbone_model(model_name='gigapath', local_weight_path=None,
+                             ROI_feature_dim=1536, **kwargs):
     if model_name == 'gigapath':
         slide_backbone = gigapath_slide_enc12l768d(in_chans=ROI_feature_dim, global_pool=False, **kwargs)
         print("Slide encoder param #", sum(p.numel() for p in slide_backbone.parameters()))
 
+        # download the weights
         if local_weight_path is None:
             # Hugging Face API token
             os.environ["HF_TOKEN"] = "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"
-            hf_hub= "hf_hub:prov-gigapath/prov-gigapath"
+            hf_hub = "hf_hub:prov-gigapath/prov-gigapath"
             hub_name = hf_hub.split(":")[1]
-            local_dir= os.path.join(os.path.expanduser("~"), ".cache/")
+            local_dir = os.path.join(os.path.expanduser("~"), ".cache/")
 
             huggingface_hub.hf_hub_download(hub_name, filename="slide_encoder.pth",
                                             local_dir=local_dir, force_download=True)
@@ -43,7 +52,74 @@ def build_WSI_backbone_model(model_name='gigapath',local_weight_path=None,ROI_fe
         return slide_backbone
 
 
-def build_WSI_task_model(model_name='gigapath',local_weight_path=None,ROI_feature_dim=1536):
-    slide_backbone = build_WSI_backbone_model(model_name,local_weight_path,ROI_feature_dim)
-    # todo build task heads
-    return slide_backbone
+class MTL_module_baseline(nn.Module):
+    def __init__(self, MTL_token_num, latent_feature_dim):
+        super().__init__()
+        self.MTL_token_num, self.latent_feature_dim = MTL_token_num, latent_feature_dim
+        self.layer = nn.Linear(latent_feature_dim, MTL_token_num * latent_feature_dim)
+
+    def forward(self, latent_features):
+        MTL_tokens = self.layer(latent_features).view(-1, self.MTL_token_num, self.latent_feature_dim)
+        return MTL_tokens
+
+
+class MTL_Model_builder(nn.Module):
+    def __init__(self, backbone: nn.Module,
+                 MTL_module: Optional[nn.Module] = None,
+                 MTL_heads: List[nn.Module] = [nn.Identity(), nn.Identity(), nn.Identity()],
+                 embed_dim: int = 768, latent_feature_dim: int = 128):
+        """
+        :param backbone: slide-level modeling model
+        :param MTL_module: MTL model projecting the features to multiple task heads
+        :param MTL_heads: the list of multiple WSI-level task heads for each task
+        :param embed_dim: feature dim for MTL heads and WSI level modeling model
+        :param latent_feature_dim: feature dim for MTL modeling
+        """
+        super().__init__()
+
+        self.backbone = backbone  # the output feature is [B, embed_dim]
+
+        if latent_feature_dim == embed_dim:
+            self.embedding_converter = nn.Identity()
+        else:
+            self.embedding_converter = nn.Linear(embed_dim, latent_feature_dim)
+
+        self.MTL_token_num = len(MTL_heads)
+        assert self.MTL_token_num > 0, "MTL_heads cannot be empty."
+
+        if MTL_module is None:
+            self.MTL_module = MTL_module_baseline(self.MTL_token_num, latent_feature_dim)
+        else:
+            self.MTL_module = MTL_module
+
+        self.MTL_heads = nn.ModuleList(MTL_heads)
+
+    def forward(self, image_features, img_coords):
+        """
+        Forward pass for the MTL Transformer.
+
+        :param image_features: Tensor of shape [B, N, feature_dim],
+                               where B is batch size, N is the number of patches/features per slide.
+        :return: List of task predictions, where each element in the list corresponds to a task and has
+                 shape [B, output_dim] (output_dim may vary depending on the task).
+        """
+        slide_latent = self.backbone(image_features, img_coords)[0]  # fixme take from the list of embeddings
+        print(slide_latent)
+        MTL_tokens = self.MTL_module(self.embedding_converter(slide_latent))
+
+        WSI_tasks_pred = []
+        for idx, head in enumerate(self.MTL_heads):
+            task_pred = head(MTL_tokens[:, idx])
+            WSI_tasks_pred.append(task_pred)
+
+        return WSI_tasks_pred
+
+
+def build_WSI_task_model(model_name='gigapath', local_weight_path=None, ROI_feature_dim=1536,
+                         MTL_heads=None,latent_feature_dim=128):
+    assert MTL_heads is not None
+    slide_backbone = build_WSI_backbone_model(model_name, local_weight_path, ROI_feature_dim)
+    MTL_Model = MTL_Model_builder(slide_backbone, MTL_module=None, MTL_heads=MTL_heads,
+                                  embed_dim=768,latent_feature_dim=latent_feature_dim)
+
+    return MTL_Model
---
+++ ModelBase/Task_settings.py
@@ -0,0 +1,195 @@
+"""
+MTL Task settings   Script  ver： Aug 21th 01:00
+
+flexible to multiple-tasks and missing labels
+"""
+
+import os
+import json
+import copy
+import numpy as np
+import torch.nn as nn
+import yaml  # Ensure pyyaml is installed: pip install pyyaml
+
+
+def build_tasks(task_idx_list, task_idx_to_name, all_task_dict, all_MTL_heads,
+                all_criterions, all_loss_weight, all_class_num, all_task_describe):
+    # build tasks
+    task_dict = {}
+    MTL_heads = []
+    criterions = []
+    loss_weight = []
+    class_num = []
+    task_describe = {}
+
+    for idx in task_idx_list:
+        task_dict[task_idx_to_name[idx]] = all_task_dict[task_idx_to_name[idx]]
+        MTL_heads.append(all_MTL_heads[idx])
+        criterions.append(all_criterions[idx])
+        loss_weight.append(all_loss_weight[idx])
+        class_num.append([all_class_num[idx]])
+        if task_idx_to_name[idx] in all_task_describe:
+            task_describe[task_idx_to_name[idx]] = all_task_describe[task_idx_to_name[idx]]
+
+    return task_dict, MTL_heads, criterions, loss_weight, class_num, task_describe
+
+
+def task_filter_auto(task_config_path, latent_feature_dim=128):
+    """Auto task filter defined by json files, currently used by Shangqing's TCGA dataset
+
+    Args:
+        task_config_path (str): task_settings_path/task_configs.yaml
+        latent_feature_dim (int, optional): _description_. Defaults to 768.
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    try:
+        with open(task_config_path, 'r') as file:
+            config = yaml.safe_load(file)
+            task_idx_or_name_list = config.get('tasks_to_run')
+            all_task_dict = config.get('all_task_dict')
+            all_task_one_hot_describe = config.get('one_hot_table')
+    except:
+        raise  # no task-settings folder for the dataset
+    else:
+        # Generate task_idx_to_name, task_name_to_idx, all_class_num, all_loss_weight, all_criterions
+        idx = 0
+        task_name_to_idx = {}  # task name to idx list
+        task_idx_to_name = []  # task idx to name list
+        all_class_num = []  # class number list
+        all_loss_weight = []  # todo: need to allow manually config in the future, maybe config in yaml file?
+        all_criterions = []  # task loss
+        all_MTL_heads = []  # MTL heads list
+        for col in all_task_dict:
+            if all_task_dict[col] == 'float':
+                all_task_dict[col] = float
+                all_class_num.append(0)
+                all_criterions.append(nn.L1Loss(size_average=None, reduce=None))
+                all_MTL_heads.append(nn.Linear(latent_feature_dim, 1))
+            elif all_task_dict[col] == 'list':
+                all_task_dict[col] = list
+                all_class_num.append(len(all_task_one_hot_describe[col]))
+                all_criterions.append(nn.CrossEntropyLoss())
+                # pred (type: float): [Batch, cls], GT (type: long int): [Batch]
+                # (content of GT should stack together, which means their format should be the same)
+                all_MTL_heads.append(nn.Linear(latent_feature_dim, all_class_num[idx]))
+            else:
+                raise ValueError('Not valid data type!')
+
+            task_name_to_idx[col] = idx
+            task_idx_to_name.append(col)
+            all_loss_weight.append(1.0)
+            idx += 1
+
+        # support both index and text
+        if type(task_idx_or_name_list[0]) == int:
+            task_idx_list = task_idx_or_name_list
+        else:
+            task_idx_list = [task_name_to_idx[name] for name in task_idx_or_name_list]
+
+        return build_tasks(task_idx_list, task_idx_to_name, all_task_dict, all_MTL_heads, all_criterions,
+                           all_loss_weight, all_class_num, all_task_one_hot_describe)
+
+
+# base on the task dict to convert the task idx of model output
+class task_idx_converter:
+    def __init__(self, old_task_dict, new_task_dict):
+        idx_dict = {}
+        idx_project_dict = {}
+        for old_idx, key in enumerate(old_task_dict):
+            idx_dict[key] = old_idx
+        for new_idx, key in enumerate(new_task_dict):
+            idx_project_dict[new_idx] = idx_dict[key]
+
+        self.idx_project_dict = idx_project_dict
+
+    def __call__(self, new_idx):
+        back_to_old_idx = self.idx_project_dict[new_idx]
+        return back_to_old_idx
+
+
+def listed_onehot_dic_to_longint_name_dic(name_onehot_list_dic):
+    """
+    converting name_onehot_list_dic to longint_name_dic
+    Example
+    name_onehot_list_dic = {'3RD': [0, 0, 0, 0, 1],
+                            '4TH': [0, 0, 0, 1, 0],
+                            '5TH': [0, 0, 1, 0, 0],
+                            '6TH': [0, 1, 0, 0, 0],
+                            '7TH': [1, 0, 0, 0, 0]}
+
+    out
+    longint_name_dic = {4: '3RD', 3: '4TH', 2: '5TH', 1: '6TH', 0: '7TH'}
+    """
+
+    longint_name_dic = {}
+
+    for cls_name in name_onehot_list_dic:
+        listed_onehot = name_onehot_list_dic[cls_name]
+        long_int = np.array(listed_onehot).argmax()
+        longint_name_dic[long_int] = cls_name
+
+    return longint_name_dic
+
+
+class result_recorder:
+    def __init__(self, task_dict, task_describe, batch_size=1, total_size=10, runs_path=None):
+        assert runs_path is not None
+        self.runs_path = runs_path
+
+        self.task_dict = task_dict
+        self.batch_size = batch_size
+        self.total_size = total_size
+
+        # set up the indicators
+        self.longint_to_name_dic_for_all_task = {}
+        self.task_template = {}
+        self.task_idx_to_key = {}
+        task_idx = 0
+
+        for key in task_dict:
+            self.task_template[key] = {"pred": None, "label": None}
+
+            self.task_idx_to_key[task_idx] = key
+            task_idx += 1
+
+            if task_dict[key] == list:
+                self.longint_to_name_dic_for_all_task[key] = listed_onehot_dic_to_longint_name_dic(task_describe[key])
+
+        # set up the sample list
+        self.record_samples = {}
+        self.data_iter_step = 0
+
+    def add_step(self, data_iter_step):
+        self.data_iter_step = data_iter_step
+        for i in range(self.batch_size):
+            record_idx = data_iter_step * self.batch_size + i
+            if record_idx == self.total_size:
+                break  # now at the next of last sample
+            else:
+                self.record_samples[record_idx] = copy.deepcopy(self.task_template)
+
+    def add_data(self, batch_idx, task_idx, pred, label=None):
+        record_idx = self.data_iter_step * self.batch_size + batch_idx
+
+        key = self.task_idx_to_key[task_idx]
+        # rewrite template
+        if self.task_dict[key] == list:
+            self.record_samples[record_idx][key]["pred"] = self.longint_to_name_dic_for_all_task[key][pred]
+            self.record_samples[record_idx][key]["label"] = \
+                self.longint_to_name_dic_for_all_task[key][label] if label is not None else None
+        else:  # reg
+            self.record_samples[record_idx][key]["pred"] = pred
+            self.record_samples[record_idx][key]["label"] = label if label is not None else None
+
+    def finish_and_dump(self, tag='test_'):
+        # save json_log  indent=2 for better view
+        log_path = os.path.join(self.runs_path, str(tag) + 'predication.json')
+        json.dump(self.record_samples, open(log_path, 'w'), ensure_ascii=False, indent=2)
+
+# todo Cell task recorder
